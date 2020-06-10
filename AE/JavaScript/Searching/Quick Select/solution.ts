922cccb65c273aac8b00aa08c8bf134f4c2c58bd
Smaran Harihar <smaran.harihar@gmail.com>
2020-06-09 17:31:32 -0700

    Quickselect

---
+++ AE/JavaScript/Searching/Quick Select/solution.ts
@@ -0,0 +1,29 @@
+function quickselect(array, k) {
+    // Write your code here.
+    let index = quickSort(array, 0, array.length-1);
+    while(index !== k-1) {
+        index = (index > k-1) ? quickSort(array, 0, index-1) : 
+        quickSort(array, index+1, array.length-1);
+    }
+    return array[index];
+}
+
+function quickSort(array, leftM, pivotIdx) {
+    let rightM = pivotIdx-1;
+    while (leftM <= rightM) {
+        const left = array[leftM];
+        const right = array[rightM];
+        const pivot = array[pivotIdx];
+        if (left <= pivot) {
+            leftM++;
+        } else if (right > pivot) {
+            rightM--;
+        } else if (left > pivot && right < pivot) {
+            [array[leftM], array[rightM]] = [array[rightM], array[leftM]];
+            leftM++;
+            rightM--;
+        }
+    }
+    [array[pivotIdx], array[leftM]] = [array[leftM], array[pivotIdx]];
+    return leftM;
+}  
